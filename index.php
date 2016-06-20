17f01db1a651e6fd2c08966481b038b12e82cfa2
Albert Stepantsev <test@pr-of-it.ru>
2016-06-20 19:56:40 +0300

    Седьмой урок урок

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 require __DIR__ . '/autoload.php';
 
+var_dump( 1 <=> 2 ); // -1
+var_dump( 2 <=> 1 ); // 1
+var_dump( 2 <=> 2 ); // 0
+
+$a = [1, 3, 7, 2, 4, 8, 0];
+usort($a, function ($v1, $v2) {
+    return $v1->price <=> $v2->price;
+});
+var_dump($a);
+
+
 /*
 $url = $_SERVER['REQUEST_URI'];
 $parts = explode('/', $url);
@@ -16,7 +27,7 @@ try {
     $actionMethodName = 'action' . ucfirst($action);
     $controller->$actionMethodName();
 
-} catch ( PDOException $e )
+} catch ( Exception $e )
 {
     echo 'Ошибка: ' . $e->getMessage();
 }
